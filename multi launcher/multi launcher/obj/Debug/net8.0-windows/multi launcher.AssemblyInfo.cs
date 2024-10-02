d3030c021c2ab3e2750b9cda1a4a9334d0e6e557
Lolkinglolof <Torben.Thiemer@gmail.com>
2024-10-02 16:58:38 +0200

    idk but more change i guess

--- multi launcher/multi launcher/obj/Debug/net8.0-windows/multi launcher.AssemblyInfo.cs
+++ multi launcher/multi launcher/obj/Debug/net8.0-windows/multi launcher.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("multi launcher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+069e754d29293f26aa5508659b648cb4ee92889d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dd59cfdb0c66d3f9180fcf500117306377070af")]
 [assembly: System.Reflection.AssemblyProductAttribute("multi launcher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("multi launcher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- multi launcher/multi launcher/the-window.Designer.cs
+++ multi launcher/multi launcher/the-window.Designer.cs
@@ -28,12 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "the_window";
+            button1 = new Button();
+            label1 = new Label();
+            panel1 = new Panel();
+            button2 = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(135, 6);
+            button1.Name = "button1";
+            button1.Size = new Size(129, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Check for games";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 6);
+            label1.Name = "label1";
+            label1.Size = new Size(130, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Placeholder.Name";
+            label1.Click += label1_Click;
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            panel1.AutoSize = true;
+            panel1.Controls.Add(button2);
+            panel1.Location = new Point(135, 41);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(666, 640);
+            panel1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(402, 299);
+            button2.Name = "button2";
+            button2.Size = new Size(185, 29);
+            button2.TabIndex = 0;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // the_window
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "the_window";
+            Text = "the_window";
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Label label1;
+        private Panel panel1;
+        private Button button2;
     }
 }
