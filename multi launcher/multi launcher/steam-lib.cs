5dad7a1b998d35e08f371352fb140856711269e2
Lolkinglolof <Torben.Thiemer@gmail.com>
2024-10-02 14:57:01 +0200

    lib + window name
    all libs has been made for you

---
+++ multi launcher/multi launcher/steam-lib.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multi_launcher
+{
+    internal class steam_lib
+    {
+    }
+}
