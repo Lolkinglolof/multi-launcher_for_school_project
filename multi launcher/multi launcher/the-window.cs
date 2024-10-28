8d86f65f863850f9e8892889c926279df09d094e
Lolkinglolof <Torben.Thiemer@gmail.com>
2024-10-28 09:19:42 +0100

    THE SCROLL IS ALIVE!!!
    The scroll now works perfectly!

--- multi launcher/multi launcher/the-window.cs
+++ multi launcher/multi launcher/the-window.cs
@@ -324,8 +324,7 @@ namespace multi_launcher
 
         private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
         {
-            vScrollBar1.Maximum = panel1.Size.Height - panel7.Size.Height;
-            vScrollBar1.Minimum = 0;
+            
             scroll = vScrollBar1.Value;
             vScrollBar1.Value = -panel1.Location.Y;
             panel1.Location = new Point(0, 0 - scroll);
@@ -337,21 +336,30 @@ namespace multi_launcher
             // fix crash on scroll, check math before applying
             int linemove = e.Delta * SystemInformation.MouseWheelScrollLines / 120;
             int pixmove = linemove * 20;
-            panel1.Location = new Point(0, panel1.Location.Y + pixmove);
-            if (panel1.Location.Y > 0)
+            if (panel1.Height - panel7.Height <= 0)
             {
-                panel1.Location = new Point(0, 0);
-            }
-            if (panel1.Location.Y < -panel1.Height + panel7.Height)
-            {
-                panel1.Location = new Point(0, 0);
-            }
-            if (-panel1.Location.Y < vScrollBar1.Minimum || -panel1.Location.Y > vScrollBar1.Maximum)
-            {
-                
+                vScrollBar1.Maximum = panel1.Size.Height-106;
             }
             else
             {
+                vScrollBar1.Maximum = panel1.Size.Height - panel7.Size.Height;
+            }
+            
+            vScrollBar1.Minimum = 0;
+            
+            if (-panel1.Location.Y-pixmove <= vScrollBar1.Minimum)
+            {
+                panel1.Location = new Point(0, -vScrollBar1.Minimum);
+                vScrollBar1.Value = -panel1.Location.Y;
+            }
+            else if (-panel1.Location.Y-pixmove >= vScrollBar1.Maximum)
+            {
+                panel1.Location = new Point(0, -vScrollBar1.Maximum);
+                vScrollBar1.Value = -panel1.Location.Y;
+            }
+            else
+            {
+                panel1.Location = new Point(0, panel1.Location.Y + pixmove);
                 vScrollBar1.Value = -panel1.Location.Y;
             }
             
