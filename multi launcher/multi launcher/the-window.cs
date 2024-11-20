29bfef695cc2a7df204fec18047fc9d962770b0e
alvinulv <alvinulvb0159@gmail.com>
2024-11-20 14:37:10 +0100

    changed the minimum size for the window

--- multi launcher/multi launcher/the-window.cs
+++ multi launcher/multi launcher/the-window.cs
@@ -13,6 +13,7 @@
         {
             InitializeComponent();
             general.ReloadGames(panel1);
+            this.MinimumSize = new Size(1160, 600);
         }
 
         private void button1_Click(object sender, EventArgs e)
