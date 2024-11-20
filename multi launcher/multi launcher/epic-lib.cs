a5f180cfd69c76710b448e817c3b10e109a3c917
Patrick <63421519+RexStarGame@users.noreply.github.com>
2024-11-20 13:52:44 +0100

    Update
    epic work

--- multi launcher/multi launcher/epic-lib.cs
+++ multi launcher/multi launcher/epic-lib.cs
@@ -10,7 +10,42 @@ namespace multi_launcher
     {
         // Stedet hvor Epic Games manifesterne ligger
         public static readonly string EpicGamesManifestPath = @"C:\ProgramData\Epic\EpicGamesLauncher\Data\Manifests";
+        public static string EpicLocator()
+        {
+            string[] l;
+            string[] ll;
+            string Epicloc;
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            for (int i = 0; i < drives.Length; i++)
+            {
 
+                l = Directory.GetDirectories(drives[i].Name, "*(x86)");
+
+                for (int j = 0; j < l.Length; j++)
+                {
+                    Debug.WriteLine(l[j]);
+                    ll = Directory.GetDirectories(l[j], "Epic*");
+
+                    for (int k = 0; k < ll.Length; k++)
+                    {
+                        Debug.WriteLine(ll[k]);
+                        Epicloc = ll[k];
+                        return Epicloc;
+                    }
+                }
+                if (Directory.GetDirectories(drives[i].Name, "Epic*").Length == 1)
+                {
+                    for (int j = 0; j < Directory.GetDirectories(drives[i].Name, "Epic*").Length; j++)
+                    {
+                        Epicloc = Directory.GetDirectories(drives[i].Name, "Epic*")[1];
+                        return Epicloc;
+                    }
+                }
+                // possibly take another popular place later
+
+            }
+            return null;
+        }
         //public ListBox listBoxGames;
         public static string[] ManiFiles()
         {
@@ -74,10 +109,10 @@ namespace multi_launcher
         {
             string? image = null;
 
-      /*
-            string sl = ();
-            Debug.WriteLine(sl + "/appcache/librarycache");
-            string? image = Directory.GetFiles(sl + '/' + "appcache/librarycache", id + '*' + type + '*').FirstOrDefault();¨*/
+      
+            string el = EpicLocator();
+            Debug.WriteLine(el + "\\Launcher\\Portal\\SysFiles");
+            image = Directory.GetFiles(el + "\\Launcher\\Portal\\SysFiles", id + '*' + type + '*').FirstOrDefault();
             return image ?? ".\\shit_yourself.png";
             
         
